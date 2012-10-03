7810228e0244da854110c53bde9f10342cdb4030
Ivan Ostafiychuk <iostaf@gmail.com>
2012-10-03 18:16:43 +0300

    Created empty skeleton for Students application.

---
+++ Students/StudentsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  StudentsAppDelegate.h
+//  Students
+//
+//  Created by Ivan on 03.10.12.
+//  Copyright (c) 2012 Ivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StudentsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Students/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Students
+//
+//  Created by Ivan on 03.10.12.
+//  Copyright (c) 2012 Ivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "StudentsAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([StudentsAppDelegate class]));
+    }
+}
